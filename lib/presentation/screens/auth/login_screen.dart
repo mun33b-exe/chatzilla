681b8f1874baeccaad08a1d9acbfa851098e4b1e
Muhammad Muneeb Ur Rehman <m.muneeburrehmann@gmail.com>
2025-05-09 19:59:06 +0500

    Login Screen

--- lib/presentation/screens/auth/login_screen.dart
+++ lib/presentation/screens/auth/login_screen.dart
@@ -1,3 +1,5 @@
+import 'package:chatzilla/core/common/custom_button.dart';
+import 'package:chatzilla/core/common/custom_text_field.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -8,9 +10,108 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final _emailFocus = FocusNode();
+  final _passwordFocus = FocusNode();
+
+  bool _isPasswordVisible = false;
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    _emailFocus.dispose();
+    _passwordFocus.dispose();
+    super.dispose();
+  }
+
+  String? _validateEmail(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter your email address';
+    }
+    final emailRegex = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+    if (!emailRegex.hasMatch(value)) {
+      return 'Please enter a valid email address (e.g., example@email.com)';
+    }
+    return null;
+  }
+
+  String? _validatePassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter a password';
+    }
+    if (value.length < 6) {
+      return 'Password must be at least 6 characters long';
+    }
+    return null;
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: SafeArea(
+        child: Form(
+          child: Column(
+            children: [
+              const SizedBox(height: 30),
+              Text(
+                "Welcome Back",
+                style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                "Login to your account",
+                style: Theme.of(
+                  context,
+                ).textTheme.bodyLarge?.copyWith(color: Colors.grey),
+              ),
+              const SizedBox(height: 30),
+              CustomTextField(
+                controller: emailController,
+                hintText: "Email",
+                focusNode: _emailFocus,
+                validator: _validateEmail,
+                prefixIcon: const Icon(Icons.email_outlined),
+              ),
+              const SizedBox(height: 16),
+              CustomTextField(
+                controller: passwordController,
+                focusNode: _passwordFocus,
+                validator: _validatePassword,
+                hintText: "Password",
+                prefixIcon: const Icon(Icons.lock_outline),
+                obscureText: !_isPasswordVisible,
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      _isPasswordVisible = !_isPasswordVisible;
+                    });
+                  },
+                  icon: Icon(
+                    _isPasswordVisible
+                        ? Icons.visibility_off
+                        : Icons.visibility,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: CustomButton(
+          onPressed: () {
+            // handleSignIn();
+          },
+          text: "Login",
+        ),
+      ),
     );
   }
 }
