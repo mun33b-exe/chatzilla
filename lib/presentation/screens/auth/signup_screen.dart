5cb530cb556b42e870d7aa359eb5ccaeecb9698b
Muhammad Muneeb Ur Rehman <m.muneeburrehmann@gmail.com>
2025-05-10 02:29:47 +0500

    SignUp screen added

--- lib/presentation/screens/auth/signup_screen.dart
+++ lib/presentation/screens/auth/signup_screen.dart
@@ -1,34 +1,27 @@
-import 'package:chatzilla/presentation/screens/auth/signup_screen.dart';
-import 'package:flutter/gestures.dart';
-import 'package:flutter/services.dart';
 import 'package:chatzilla/config/theme/app_theme.dart';
 import 'package:chatzilla/core/common/custom_button.dart';
 import 'package:chatzilla/core/common/custom_text_field.dart';
+import 'package:chatzilla/presentation/screens/auth/login_screen.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({super.key});
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
+  State<SignupScreen> createState() => _SignupScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _SignupScreenState extends State<SignupScreen> {
   final _formKey = GlobalKey<FormState>();
   final TextEditingController emailController = TextEditingController();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   final _emailFocus = FocusNode();
   final _passwordFocus = FocusNode();
-
   bool _isPasswordVisible = false;
-  @override
-  void dispose() {
-    emailController.dispose();
-    passwordController.dispose();
-    _emailFocus.dispose();
-    _passwordFocus.dispose();
-    super.dispose();
-  }
 
   String? _validateEmail(String? value) {
     if (value == null || value.isEmpty) {
@@ -51,6 +44,15 @@ class _LoginScreenState extends State<LoginScreen> {
     return null;
   }
 
+  void dispose() {
+    emailController.dispose();
+    nameController.dispose();
+    usernameController.dispose();
+    phoneController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -59,14 +61,16 @@ class _LoginScreenState extends State<LoginScreen> {
           child: SingleChildScrollView(
             child: Column(
               children: [
-                const SizedBox(height: 170),
+                const SizedBox(height: 70),
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 25.0),
                   child: Align(
                     alignment: Alignment.centerLeft,
                     child: Text(
-                      "Welcome Back",
-                      style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                      "Create Account",
+                      style: Theme.of(
+                        context,
+                      ).textTheme.headlineMedium?.copyWith(
                         fontWeight: FontWeight.bold,
                         color: AppTheme.primaryColor,
                       ),
@@ -79,22 +83,47 @@ class _LoginScreenState extends State<LoginScreen> {
                   child: Align(
                     alignment: Alignment.centerLeft,
                     child: Text(
-                      "Login to your account",
+                      "Please fill in the details below to continue",
                       style: Theme.of(
                         context,
                       ).textTheme.bodyLarge?.copyWith(color: Colors.grey[600]),
                     ),
                   ),
                 ),
-                const SizedBox(height: 50),
+                const SizedBox(height: 30),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: CustomTextField(
+                    controller: nameController,
+                    hintText: "Name",
+                    prefixIcon: const Icon(Icons.person_outline),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: CustomTextField(
+                    controller: usernameController,
+                    hintText: "Username",
+                    prefixIcon: const Icon(Icons.alternate_email_outlined),
+                  ),
+                ),
+                const SizedBox(height: 30),
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 25.0),
                   child: CustomTextField(
                     controller: emailController,
                     hintText: "Email",
-                    focusNode: _emailFocus,
-                    validator: _validateEmail,
                     prefixIcon: const Icon(Icons.email_outlined),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: CustomTextField(
+                    controller: phoneController,
+                    hintText: "Phone",
+                    prefixIcon: const Icon(Icons.phone_outlined),
                   ),
                 ),
                 const SizedBox(height: 30),
@@ -124,17 +153,17 @@ class _LoginScreenState extends State<LoginScreen> {
                 const SizedBox(height: 60),
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: CustomButton(onPressed: () {}, text: "Login"),
+                  child: CustomButton(onPressed: () {}, text: "Sign Up"),
                 ),
-                const SizedBox(height: 30),
+                const SizedBox(height: 20),
                 Center(
                   child: RichText(
                     text: TextSpan(
-                      text: "Don't have an account?  ",
+                      text: "Already have an account?  ",
                       style: TextStyle(color: Colors.grey[600]),
                       children: [
                         TextSpan(
-                          text: "Sign Up",
+                          text: "Log In",
                           style: TextStyle(
                             color: AppTheme.primaryColor,
                             fontWeight: FontWeight.bold,
@@ -146,7 +175,7 @@ class _LoginScreenState extends State<LoginScreen> {
                                   Navigator.push(
                                     context,
                                     MaterialPageRoute(
-                                      builder: (context) => const SignupScreen(),
+                                      builder: (context) => const LoginScreen(),
                                     ),
                                   );
                                 },
@@ -155,7 +184,6 @@ class _LoginScreenState extends State<LoginScreen> {
                     ),
                   ),
                 ),
-                const SizedBox(height: 20),
               ],
             ),
           ),
